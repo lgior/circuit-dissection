5f5fdafdcf89c8b1cee39819ce5d47ba467bc903
giordano ramos <gio@hms.harvard.edu>
2023-07-17 15:43:01 -0400

    Save plot from analysis of silencing representations

--- insilico_experiments/analyze_silencing_exps.py
+++ insilico_experiments/analyze_silencing_exps.py
@@ -41,14 +41,16 @@ rootdir = r"C:\Users\giordano\Documents\Data"  # r"E:\Monkey_Data\BigGAN_Optim_T
 rootdir = r"M:\Data"  # r"E:\Monkey_Data\BigGAN_Optim_Tune_tmp"
 rootdir = r"C:\Users\gio\Data"  # since my home folder got full
 layer_str = '.classifier.Linear6'
-# layer_str = '.Linearfc'
+layer_str = '.Linearfc'
 unit_idx = 373#398# imagenet 373  # ecoset 13, 373, 14, 12, 72, 66, 78
 unit_pattern = 'alexnet-eco-080.*%s_%s' % (layer_str, unit_idx)
 unit_pattern = 'alexnet_.*%s_%s' % (layer_str, unit_idx)
 # unit_pattern = 'resnet50_%s_%s' % (layer_str, unit_idx)
-# unit_pattern = 'resnet50_linf0.5_%s_%s' % (layer_str, unit_idx)
+unit_pattern = 'resnet50_linf8_%s_%s' % (layer_str, unit_idx)
 perturbation_pattern = '_kill_topFraction_'
 # unit_pattern += perturbation_pattern
+figdir = os.path.join('C:', 'Users', 'gio', 'Data', 'figures', 'silencing')
+os.makedirs(figdir, exist_ok=True)
 # %%
 original_dir = [idir for idir in next(os.walk(rootdir))[1] if re.match(unit_pattern + '$', idir)]
 data_dirs = [idir for idir in next(os.walk(rootdir))[1] if re.match(unit_pattern + perturbation_pattern, idir)]
@@ -123,8 +125,8 @@ max_per_condition_group = df.groupby(['type', 'strength']).apply(lambda group: g
 max_per_condition_df = pd.DataFrame({'max_index': max_per_condition_group}).reset_index()
 #%%
 # Alternative plotting code. TODO remove it
-# n_types = len(pd.unique(max_per_condition_df.sort_values(['type', 'strength']).type))
-# max_n_ims_type = max_per_condition_df.sort_values(['type', 'strength']).groupby('type').apply(len).max()
+n_types = len(pd.unique(max_per_condition_df.sort_values(['type', 'strength']).type))
+max_n_ims_type = max_per_condition_df.sort_values(['type', 'strength']).groupby('type').apply(len).max()
 # for ii, (name, group) in enumerate(max_per_condition_df.sort_values(['type', 'strength']).groupby('type')):
 #     # print(group)
 #     print(name)
@@ -162,7 +164,10 @@ def plot_images_across_silencing_conditions(max_df, plot_weighted_im_mean=False)
                 image = ToPILImage()(max_per_condition_im_list[list_index])  # index is the 0th column of the row.
                 # image = ToPILImage()(perturbation_data_dict['images'][list_index])  # index is the 0th column of the row.
             plt.imshow(image)  # index is the 0th column of the row.
-            plt.title('strength: {},\n score:{:0.2f}'.format(df.strength[merged_list_index], df.scores[merged_list_index]))
+            if ii == 0 and jj == 0:
+                plt.title('(strength: {},\nscore: {:0.2f})'.format(df.strength[merged_list_index], df.scores[merged_list_index]))
+            else:
+                plt.title('({}, {:0.2f})'.format(df.strength[merged_list_index], df.scores[merged_list_index]))
             if jj == 0:
                 ax.set_ylabel(name)
                 ax.set_xticks([])
@@ -176,7 +181,7 @@ def plot_images_across_silencing_conditions(max_df, plot_weighted_im_mean=False)
         plt.sca(ax)
         plt.axis('off')
         # maybe remove instead of setting it off
-    plt.show()
+    # plt.show()
 
 # plot max images
 fig = plt.figure(figsize=(20., 8.))
@@ -185,6 +190,12 @@ grid = ImageGrid(fig, 111,  # similar to subplot(111)
                  axes_pad=0.4,  # pad between axes in inch.
                  )
 plot_images_across_silencing_conditions(max_df, plot_weighted_im_mean=False)
+plt.savefig(os.path.join(figdir, '%s__preferred_ims_best.pdf' % original_dir[0]), dpi=300, format='pdf', metadata=None,
+            bbox_inches=None, pad_inches=0.1,
+            facecolor='auto', edgecolor='auto',
+            backend=None, transparent=True
+           )
+plt.show()
 
 # plot average images TODO correct weighted image to proper range e.g. 0-1
 fig = plt.figure(figsize=(20., 8.))
@@ -193,8 +204,12 @@ grid = ImageGrid(fig, 111,  # similar to subplot(111)
                  axes_pad=0.4,  # pad between axes in inch.
                  )
 plot_images_across_silencing_conditions(max_df, plot_weighted_im_mean=True)
-
-
+plt.savefig(os.path.join(figdir, '%s__preferred_ims_mean.pdf' % original_dir[0]), dpi=300, format='pdf', metadata=None,
+            bbox_inches=None, pad_inches=0.1,
+            facecolor='auto', edgecolor='auto',
+            backend=None, transparent=True
+           )
+plt.show()
 #%%
 
 for i_cond, (scores, images) in enumerate(zip(perturbation_data_dict['scores'], perturbation_data_dict['images'])):
@@ -226,10 +241,14 @@ for ii, (name, group) in enumerate(df.sort_values(['type', 'strength'], ascendin
         print(row_data)
         plt.sca(ax)
         list_index = row_data.old_index
-        top9_im_grid = get_top_n_im_grid(scores=perturbation_data_dict['scores'][list_index],
+        top9_im_grid = anevo.get_top_n_im_grid(scores=perturbation_data_dict['scores'][list_index],
                                          images=perturbation_data_dict['images'][list_index], top_n=9)
         plt.imshow(top9_im_grid)  # index is the 0th column of the row.
-        plt.title('strength: {},\n score:{:0.2f}'.format(row_data.strength, row_data.scores))
+        if ii == 0 and jj == 0:
+            plt.title('(strength: {},\nscore: {:0.2f})'.format(row_data.strength, row_data.scores))
+        else:
+            plt.title('({}, {:0.2f})'.format(row_data.strength, row_data.scores))
+
         if jj == 0:
             ax.set_ylabel(name)
             ax.set_xticks([])
@@ -243,32 +262,69 @@ for ax in grid[max_n_ims_type * ii + jj + 1:]:
     plt.sca(ax)
     plt.axis('off')
     # maybe remove instead of setting it off
+
+plt.savefig(os.path.join(figdir, '%s__preferred_ims_top9.pdf' % original_dir[0]), dpi=300, format='pdf', metadata=None,
+            bbox_inches=None, pad_inches=0.1,
+            facecolor='auto', edgecolor='auto',
+            backend=None, transparent=True
+           )
 plt.show()
 
 
 # %%
+params = {
+   'axes.labelsize': 16,
+   'font.size': 28,
+   'font.family': 'Arial',
+   'legend.fontsize': 16,
+   'xtick.labelsize': 22,
+   'ytick.labelsize': 22,
+   'text.usetex': False,
+   'figure.figsize': [5, 5]
+   }
+# rcParams.update(params)
+
+with mpl.rc_context(params):
+    fig, ax = plt.subplots(figsize=(5, 5), dpi=300)
+    # Plot the strength of response vs strength of input perturbation
+    # fig, ax = plt.subplots(figsize=(8,6))
+    # df.groupby('type').plot(x='strength', y='scores', kind='scatter', ax=ax)
+    ax_line = sns.lineplot(data=df, x='strength', y='scores', hue='type', errorbar=('ci', 95), markers='o',
+                           hue_order=['abs', 'inh', 'exc', 'none'],
+                           lw=4, err_kws={'edgecolor': None})
+    ax_scatter = sns.scatterplot(data=df, x=anevo.jitter(df.strength, 0.008), y='scores', hue='type', alpha=0.5,
+                                 hue_order=['abs', 'inh', 'exc', 'none'],
+                                 ec='w', s=80)
+    # sns.stripplot(df, x='strength', y='scores', hue='type', alpha=0.5) # this makes x axis categorical
+    # sns.violinplot(df, x='strength', y='scores', hue='type', inner='points')
+    # sns.violinplot(data=df, x="strength", y="scores", inner="points", hue='type')
+    # sns.violinplot(data=df[df.type == 'abs'], x="strength", y="scores", inner="points")
+    # sns.violinplot(data=df, x='strength', y="scores", hue='type', inner="points", face=0.9)
+    h_leg_scatter, label_scatter = ax_scatter.get_legend_handles_labels()
+    plt.legend(handles=list(zip(h_leg_scatter[:-4], h_leg_scatter[-4:])), labels=label_scatter[:-4], loc='best',
+               labelspacing=0.2,  handlelength=1, fontsize=10)
+    # plt.legend()
+
+    plt.title(original_dir[0])
+    # ax.set_title('Testing', font=fpath)
+    # prop = fm.FontProperties(fname=f.name)
+    # ax.set_title('this is a special font:\n%s' % github_url, fontproperties=prop)
+    plt.xlabel('Total silencing strength')
+    plt.ylabel('Response (a.u.)')
+    # plt.ylim([0, 1])
+    # ax.set_xticks([0.2, 0.4, 0.6, 0.8, 1.0])
+    # ax.set_yticks([0, 0.5, 1])
+    fig.tight_layout()
+
+    plt.savefig(os.path.join(figdir, '%s__neuron_response.pdf' % original_dir[0]), dpi=300, format='pdf', metadata=None,
+            bbox_inches=None, pad_inches=0.1,
+            facecolor='auto', edgecolor='auto',
+            backend=None, transparent=True
+           )
+    plt.show()
+# %%
 
 
-def jitter(values, scale):
-    return values + np.random.normal(0, scale, values.shape)
-
-
-# Plot the strength of response vs strength of input perturbation
-# fig, ax = plt.subplots(figsize=(8,6))
-# df.groupby('type').plot(x='strength', y='scores', kind='scatter', ax=ax)
-ax_line = sns.lineplot(data=df, x='strength', y='scores', hue='type', errorbar=('ci', 95), markers='o',
-                       hue_order=['abs', 'inh', 'exc', 'none'])
-ax_scatter = sns.scatterplot(data=df, x=jitter(df.strength, 0.008), y='scores', hue='type', alpha=0.5,
-                             hue_order=['abs', 'inh', 'exc', 'none'])
-# sns.stripplot(df, x='strength', y='scores', hue='type', alpha=0.5) # this makes x axis categorical
-# sns.violinplot(df, x='strength', y='scores', hue='type', inner='points')
-# sns.violinplot(data=df, x="strength", y="scores", inner="points", hue='type')
-# sns.violinplot(data=df[df.type == 'abs'], x="strength", y="scores", inner="points")
-# sns.violinplot(data=df, x='strength', y="scores", hue='type', inner="points", face=0.9)
-h_leg_scatter, label_scatter = ax_scatter.get_legend_handles_labels()
-plt.legend(handles=list(zip(h_leg_scatter[:-4], h_leg_scatter[-4:])), labels=label_scatter[:-4], loc='lower left')
-# plt.legend()
-plt.show()
 
 # TODO plot the strength of the input with respect to the activations
 
